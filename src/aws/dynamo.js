32067cfd70aa0d1185d5a7824ec0dc3abd9173c1
Brayden Gibbons <brayden.gibbons66@gmail.com>
2021-06-10 08:03:04 -0600

    implement creation of dynamo tables

--- src/aws/dynamo.js
+++ src/aws/dynamo.js
@@ -1,43 +1,63 @@
 const AWS = require("aws-sdk");
-
 AWS.config.update({ region: "us-west-2" });
 
 const dynamodb = new AWS.DynamoDB();
 
-const gameObjectsParams = {
-  TableName : "GameObjects",
-  KeySchema: [       
-      { AttributeName: "game_id", KeyType: "HASH"},  //Partition key
-  ],
-  AttributeDefinitions: [       
-      { AttributeName: "game_id", AttributeType: "S" },
-  ],
-};
+async function createTable(params) {
+  try {
+    const res = await dynamodb.createTable(params).promise()
+    console.log(res)
 
-dynamodb.createTable(params, function(err, data) {
-  if (err) {
-      console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
-  } else {
-      console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+  } catch (e) {
+    if (e.name === 'ResourceInUseException') {
+      console.log(`${params.TableName} table already exists`)
+    } else {
+      console.error(e)
+    }
   }
-});
+}
 
-const GamerUsersParams = {
-  TableName : "GameUsers",
-  KeySchema: [       
-      { AttributeName: "game_id", KeyType: "HASH"},  //Partition key
-      { AttributeName: "user_name", KeyType: "RANGE" }  //Sort key
-  ],
-  AttributeDefinitions: [       
-      { AttributeName: "game_id", AttributeType: "S" },
-      { AttributeName: "user_name", AttributeType: "S" }
-  ],
-};
-
-dynamodb.createTable(params, function(err, data) {
-  if (err) {
-      console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
-  } else {
-      console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+async function createGameObjectTable() {
+  const gameObjectParams = {
+    TableName : "GameObjects",
+    KeySchema: [       
+        { AttributeName: "game_id", KeyType: "HASH"},  //Partition key
+    ],
+    AttributeDefinitions: [       
+        { AttributeName: "game_id", AttributeType: "S" },
+    ],
+    ProvisionedThroughput: {       
+      ReadCapacityUnits: 5, 
+      WriteCapacityUnits: 5
   }
-});
+  };
+
+  await createTable(gameObjectParams)
+}
+
+async function createGameUserTable() {
+  const gameUsersParams = {
+    TableName : "GameUsers",
+    KeySchema: [       
+        { AttributeName: "game_id", KeyType: "HASH"},  //Partition key
+        { AttributeName: "user_name", KeyType: "RANGE" }  //Sort key
+    ],
+    AttributeDefinitions: [       
+        { AttributeName: "game_id", AttributeType: "S" },
+        { AttributeName: "user_name", AttributeType: "S" }
+    ],
+    ProvisionedThroughput: {       
+      ReadCapacityUnits: 5, 
+      WriteCapacityUnits: 5
+  }
+  };
+
+  await createTable(gameUsersParams)
+}
+
+async function initialize() {
+  await createGameObjectTable()
+  await createGameUserTable()
+}
+
+initialize()
